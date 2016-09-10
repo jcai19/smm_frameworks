5b71d5ad05c76cb2c95e2b8859bff8716947894f
Jian Cai <jcai19@asu.edu>
2016-09-10 15:08:50 -0700

    Move dma files to common

---
+++ common/include/dma.h
@@ -0,0 +1,21 @@
+#ifndef __DMA_H__
+#define __DMA_H__
+
+#include <stdlib.h>
+
+enum TRANSDIR {SPM2MEM, MEM2SPM};
+
+#if defined(__i386__)
+
+//int dma(void *, void *, unsigned int, int) __attribute__((always_inline));
+int dma(void *, void *, unsigned int, int);
+
+#elif defined(__x86_64__)
+
+//int dma(void *, void *, unsigned long, int) __attribute__((always_inline));
+int dma(void *, void *, unsigned long, int);
+
+#endif
+
+
+#endif
